60210b3fab827489a96eb45f80031847ec720420
Inge Chiles <Inge.Chiles@gmail.com>
2024-07-11 15:59:48 -0500

    Initial Commit

---
+++ SeaCreatures/ContentView.swift
@@ -0,0 +1,23 @@
+//  ContentView.swift
+//  SeaCreatures
+//  Made with ♥︎ by Inge Chiles on 7/11/24.
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Model3D(named: "Scene", bundle: realityKitContentBundle)
+                .padding(.bottom, 50)
+
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+}
---
+++ SeaCreatures/SeaCreaturesApp.swift
@@ -0,0 +1,14 @@
+//  SeaCreaturesApp.swift
+//  SeaCreatures
+//  Made with ♥︎ by Inge Chiles on 7/11/24.
+
+import SwiftUI
+
+@main
+struct SeaCreaturesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
